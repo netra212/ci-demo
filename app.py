8a771939883b7c2a2b707666dc696dc3faa886f2
netr212 <netra200021kcbdr@gmail.com>
2024-12-27 18:57:27 +0000

    Fixerd Linting issues

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 def add(a, b):
-    return a+b
+    return a + b
+
 
 def sub(a, b): 
-    return a-b
+    return a - b
 
-result1 = add(4,5)
-result2 = sub(4,1)
+result1 = add(4, 5)
+result2 = sub(4, 1)
 
 print(result1, result2)
